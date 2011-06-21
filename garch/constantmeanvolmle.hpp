d5f5e3ec3167292c8fbf8e98579e25478974eaa3
Leon Sit <leon.sit@milliman.com>
2011-06-20 23:14:44 -0500

    add a new file

---
+++ garch/constantmeanvolmle.hpp
@@ -0,0 +1,10 @@
+#ifndef timeseries_constantmeanvolmle_hpp
+#define timeseries_constantmeanvolmle_hpp
+
+#include <estimationengine.hpp>
+
+class ConstantMeanVolMLE : public EstimationEngine{
+  
+};
+
+#endif
